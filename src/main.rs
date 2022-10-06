9a6feee7d17ee5758f2828a69e880f7f4dce5a3c
Codenameaidan <ajw137@pitt.eu>
2022-10-06 14:34:51 -0400

    Added initial version of static terrain

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use bevy::prelude::*;
+use bevy::{prelude::*,};
 
 mod credit_image;
 mod world;
 
 fn main() {
-    /*App::new()
+    /*
+    //Credits
+    App::new()
         .add_startup_system(|mut c: Commands| {
             c.spawn_bundle(Camera2dBundle::default());
         })
@@ -23,9 +25,9 @@ fn main() {
 
 fn setup(mut commands: Commands, assets: Res<AssetServer>,){
     //Generate one chunk
-    let x = world::Chunk::new();
-    world::SpawnChunk(&x, commands, assets);
-    
-            
-        
+    let mut chunk = world::Chunk::new();
+    world::spawn_chunk(&mut chunk, &mut commands, assets); 
+
+    //(Example): Destroy a single block at 3,3
+    world::destory_block(&mut chunk, &mut commands, 3, 3);
 }
--- src/world.rs
+++ src/world.rs
@@ -1,58 +1,87 @@
-//let data = vec!["sand"];
 use bevy::prelude::*;
+
 static BLOCK_MAPPING: &'static [&str] = &[
     "NONE",
     "Sandstone.png",
 ];
+const CHUNK_HEIGHT: usize = 5;
+const CHUNK_WIDTH:  usize =  40;
+
 
 pub struct Chunk{
-    tiles: [[Block; 5];8],// state = [[0u8; 4]; 6];
-    //startX: i32
+    pub blocks: [[Block; CHUNK_HEIGHT];CHUNK_WIDTH],
+    //depth_number: i32 //TODO make sure chunk generates at the correct depth (right now, all starting from 0)
 }
 
 impl Chunk{
     pub fn new() -> Self{
-        let mut c = Chunk{ tiles: [[Block {block_type:0 , id: 0}; 5];8] };
-        c.tiles[0][0] = Block{block_type: 1, id: 0};
-        c.tiles[0][2] = Block{block_type: 1, id: 0};
-        c.tiles[1][1] = Block{block_type: 1, id: 0};
-
+        //For now: populate entire Chunk with block_type 1
+        let c = Chunk{ blocks: [[Block {block_type:1 , id: Option::None}; CHUNK_HEIGHT];CHUNK_WIDTH] };
         return c;
     }
 }
 
-pub fn SpawnChunk(c: &Chunk, mut commands: Commands, assets: Res<AssetServer>){
-            
-        for (i, row) in c.tiles.iter().enumerate() {
-            for (y, col) in row.iter().enumerate() {
-                if(c.tiles[i][y].block_type == 0){
-                    continue;
-                }
-                commands.spawn()
-                .insert_bundle(
-                    SpriteBundle {
-                        texture: assets.load(BLOCK_MAPPING[c.tiles[i][y].block_type]),
-                        transform: Transform {
-                            translation: Vec3::from_array([ToWorldPoint(i), ToWorldPoint(y), 1.]),
-                            ..default()
-                        },
-                        ..default()
-                    }
-                );
-            }
-        }
-}
-
 #[derive(Copy, Clone)]
-struct Block{
-    block_type: usize,
-    id: u32
+pub struct Block{
+    pub block_type: usize,
+    pub id: Option<Entity>
 }
 
-fn ToWorldPoint(i: usize) -> f32{
+
+/// Create all blocks in chunk as actual entities (and store references to entity in chunk.blocks)
+pub fn spawn_chunk(c: &mut Chunk, commands: &mut Commands, assets: Res<AssetServer>){
+    
+    //Loop through entire chunk (2D Array)
+    for i in 0..c.blocks.len() {
+        for j in 0..c.blocks[i].len() {
+            if c.blocks[i][j].block_type == 0{
+                continue;
+            }
+            let entity = commands.spawn()
+            .insert_bundle(
+                SpriteBundle {
+                    texture: assets.load(BLOCK_MAPPING[c.blocks[i][j].block_type]),
+                    transform: Transform {
+                        translation: Vec3::from_array([to_world_point_x(i), to_world_point_y(j), 1.]),
+                        ..default()
+                    },
+                    ..default()
+                }
+            ).id();
+            c.blocks[i][j].id = Option::Some(entity);
+        }
+    }   
+}
+
+//Destroy an individual block of a chunk
+pub fn destory_block(c: &mut Chunk, commands: &mut Commands, x: usize, y:usize){
+    let opt = c.blocks[x][y].id;
+
+    match opt{
+        Some(opt) =>{
+            c.blocks[x][y] = Block{block_type: 0, id: Option::None};
+            commands.entity(opt).despawn();   
+        }
+        None => {
+
+        }
+    }
+}
+
+
+fn to_world_point_x(i: usize) -> f32{
     return (i as f32) * 32.;
 }
-
-fn ToIndex(){
-    
+fn to_world_point_y(i: usize) -> f32{
+    return - (i as f32) * 32.;
 }
+
+/*
+    fn ToIndexX(){
+        
+    }
+
+    fn ToIndexY(){
+
+    }
+*/
