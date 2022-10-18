da3bb8270530ec4be65478fc3e186115f2597b48
Nicholas Thompson <nicholasthompsonpgh@gmail.com>
2022-10-18 14:30:05 -0400

    Added UI

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use bevy::prelude::*;
 
 mod credit_image;
+mod menu;
 mod states;
 
 fn main() {
     App::new()
-        .add_startup_system(|mut c: Commands| {
-            c.spawn_bundle(Camera2dBundle::default());
-        })
+        .add_startup_system(setup)
         .add_plugins(DefaultPlugins)
         .add_plugin(states::StatePlugin)
         .add_plugin(credit_image::CreditImagePlugin)
+        .add_plugin(menu::MenuPlugin)
         .run();
+}
+
+fn setup(mut c: Commands) {
+    c.spawn_bundle(Camera2dBundle::default());
 }
--- src/states.rs
+++ src/states.rs
@@ -3,6 +3,8 @@ use bevy::prelude::*;
 /// Represents runtime "flow" of the game
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum GameState {
+    ///menu
+    Menu,
     /// Main game loop, game world exists with player
     InGame,
     /// Credits screen
@@ -12,7 +14,7 @@ pub enum GameState {
 /// Initial GameState
 impl Default for GameState {
     fn default() -> Self {
-        GameState::InGame
+        GameState::Menu
     }
 }
 
@@ -26,12 +28,14 @@ impl Plugin for StatePlugin {
     }
 }
 
+
 /// Simple system to facilitate changing GameState via F1 key
 /// TODO: This is good enough for debugging, but should be reworked eventually
 fn input_state_change(mut state: ResMut<State<GameState>>, input: Res<Input<KeyCode>>) {
     if input.just_pressed(KeyCode::F1) {
         let new_state = match *state.current() {
-            GameState::Credits => GameState::InGame,
+            GameState::Menu => GameState::InGame,
+            GameState::Credits => GameState::Menu,
             GameState::InGame => GameState::Credits,
         };
         info!(
